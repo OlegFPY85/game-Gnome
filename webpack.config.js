3e91ecf566f65ac40ee1b97f7bc4ba76f2e5b7da
OlegFPY85 <132295105+OlegFPY85@users.noreply.github.com>
2025-08-15 15:27:48 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: 'assets/[name][ext]',
-    publicPath: './'
+    publicPath: '/'
   },
   resolve: {
     fallback: {
